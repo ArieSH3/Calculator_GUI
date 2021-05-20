d1601ab56bb108f67b9b95f648fc612ce22ae528
ArieSH3 <StepDubUp@gmail.com>
2021-05-20 03:32:09 +0200

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,27 @@
 '''
-	Creating a calculator with Python and PySimpleGUI library
+	Author: Stefan Popovic
+	Date  : 20/05/2021
+
+	About : Creating a calculator with Python and PySimpleGUI library
+
 '''
+
+import PySimpleGUI as sg
+
+def main():
+	pass
+
+
+
+
+def function():
+	pass
+
+
+
+
+
+
+
+
+main()
